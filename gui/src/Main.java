f6c1dd5ca8cb5b9d8e480832227807fa39737e2d
Alex Pavlyk <sashapvk7@gmail.com>
2026-06-09 13:33:48 +0200

    finestra

--- gui/src/Main.java
+++ gui/src/Main.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame finestra = new JFrame();
         finestra.setTitle("GuildChess");
+        finestra.setSize(1600, 900);
+        finestra.setLocationRelativeTo(null);
         finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        finestra.setSize(1280, 720);
+        finestra.setResizable(false);
+        finestra.setIconImage((new ImageIcon(
+            Main.class.getResource("/logo.png")).getImage()));
+        finestra.getContentPane().setBackground(new Color(39, 35, 49));
         finestra.setVisible(true);
     }
 }
