b0da83ed850492624392fde126570144d08ef6d3
Alex Pavlyk <sashapvk7@gmail.com>
2026-06-09 12:04:48 +0200

    exit on close

--- gui/src/Main.java
+++ gui/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame finestra = new JFrame();
         finestra.setTitle("GuildChess");
+        finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         finestra.setSize(1280, 720);
         finestra.setVisible(true);
     }
