a38e19b24565f44dec424b554e2c057c74a7c3a9
Alex Pavlyk <sashapvk7@gmail.com>
2026-06-09 12:03:03 +0200

    gui

---
+++ gui/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame finestra = new JFrame();
+        finestra.setTitle("GuildChess");
+        finestra.setSize(1280, 720);
+        finestra.setVisible(true);
+    }
+}
